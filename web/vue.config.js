9b04196bf9fe4ac44d46ffee362081ee498e6c3f
Zane <chen7157783@gmail.com>
2020-08-17 00:17:03 +0800

    test

--- web/vue.config.js
+++ web/vue.config.js
@@ -3,6 +3,5 @@ module.exports = {
     lintOnSave: false,
     outputDir:__dirname + '/../server/web',
 
-    publicPath:process.env.NODE_ENV === 'production'
-        ?'/':'/'
+    publicPath:process.env.NODE_ENV === 'production'?'/':'/'
 }
