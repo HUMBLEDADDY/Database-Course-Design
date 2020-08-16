060b00e008af8e151b8fa15dae9d6fd07c2a47b4
Zane <chen7157783@gmail.com>
2020-08-16 18:06:55 +0800

    test

--- admin/src/http.js
+++ admin/src/http.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 import router from './router'
 
 const http = axios.create({
-    baseURL: process.env.VUE_APP_API_URL || '/admin/api',  
+    baseURL: process.env.NODE_ENV || '/admin/api',  
 })
 
 http.interceptors.request.use(function (config) {
